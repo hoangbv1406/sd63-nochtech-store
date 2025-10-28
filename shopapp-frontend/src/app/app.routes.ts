90451ba96c3720054b1dac59d780a287709dc4ba
hoangbv1406 <hoangbv.dev@gmail.com>
2025-10-28 10:00:00 +0700

    feat: add heading to register template and configure route

--- shopapp-frontend/src/app/app.routes.ts
+++ shopapp-frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthCallbackComponent } from './components/auth-callback/auth-callback.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'auth/google/callback', component: AuthCallbackComponent },
-    { path: 'auth/facebook/callback', component: AuthCallbackComponent }
+    { path: 'auth/facebook/callback', component: AuthCallbackComponent },
+    { path: 'register', component: RegisterComponent }
 ];
