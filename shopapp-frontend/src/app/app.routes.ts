0d8787b3a36d51b0c047b4d75c8662d6aa0a1215
hoangbv1406 <hoangbv.dev@gmail.com>
2025-10-28 10:00:00 +0700

    feat: add heading to Product-ID template and configure route

--- shopapp-frontend/src/app/app.routes.ts
+++ shopapp-frontend/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthCallbackComponent } from './components/auth-callback/auth-callback.component';
 import { RegisterComponent } from './components/register/register.component';
+import { DetailProductComponent } from './components/product-detail/product.detail.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'auth/google/callback', component: AuthCallbackComponent },
     { path: 'auth/facebook/callback', component: AuthCallbackComponent },
-    { path: 'register', component: RegisterComponent }
+    { path: 'register', component: RegisterComponent },
+    { path: 'products/:id', component: DetailProductComponent }
 ];
