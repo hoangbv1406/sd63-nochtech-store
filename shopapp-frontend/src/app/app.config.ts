32fe79cd78368dc6e0e430ad2bf5a2da26ff4923
hoangbv1406 <hoangbv.dev@gmail.com>
2025-10-30 10:00:00 +0700

    feat: register child routes and provide HttpClient & zone change detection

--- shopapp-frontend/src/app/app.config.ts
+++ shopapp-frontend/src/app/app.config.ts
@@ -1,9 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, RouterModule } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(withFetch()),
+    importProvidersFrom(RouterModule.forChild(routes)),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+  ]
 };
