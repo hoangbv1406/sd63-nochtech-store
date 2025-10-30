980649c927623ee32406cf80dd924c019a8f3cac
hoangbv1406 <hoangbv.dev@gmail.com>
2025-10-30 10:00:00 +0700

    feat: add heading to order template and configure route

--- shopapp-frontend/src/app/app.routes.ts
+++ shopapp-frontend/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './components/login/login.component';
 import { AuthCallbackComponent } from './components/auth-callback/auth-callback.component';
 import { RegisterComponent } from './components/register/register.component';
 import { DetailProductComponent } from './components/product-detail/product.detail.component';
+import { OrderComponent } from './components/order/order.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -12,5 +13,6 @@ export const routes: Routes = [
     { path: 'auth/google/callback', component: AuthCallbackComponent },
     { path: 'auth/facebook/callback', component: AuthCallbackComponent },
     { path: 'register', component: RegisterComponent },
-    { path: 'products/:id', component: DetailProductComponent }
+    { path: 'products/:id', component: DetailProductComponent },
+    { path: 'orders', component: OrderComponent }
 ];
