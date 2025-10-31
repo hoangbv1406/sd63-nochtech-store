fc1688359c902c36737ceb24bcbafbeefa7472ee
hoangbv1406 <hoangbv.dev@gmail.com>
2025-10-31 10:00:00 +0700

    feat: add admin component with sidebar, header and routing

--- shopapp-frontend/src/app/components/admin/admin.component.ts
+++ shopapp-frontend/src/app/components/admin/admin.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { BaseComponent } from '../base/base.component';
+import { UserResponse } from '../../responses/user.response';
 
 @Component({
   selector: 'app-admin',
@@ -9,5 +11,33 @@ import { RouterModule } from '@angular/router';
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss'
 })
-export class AdminComponent {
+export class AdminComponent extends BaseComponent implements OnInit {
+  userResponse?: UserResponse | null;
+
+  ngOnInit() {
+    this.userResponse = this.userService.getUserResponseFromLocalStorage();
+    if (this.router.url === '/admin') {
+      this.router.navigate(['/admin/orders']);
+    }
+  }
+
+  logout() {
+    this.userService.removeUserFromLocalStorage();
+    this.tokenService.removeToken();
+    this.userResponse = this.userService.getUserResponseFromLocalStorage();
+    this.router.navigate(['/']);
+  }
+
+  showAdminComponent(componentName: string): void {
+    if (componentName === 'orders') {
+      this.router.navigate(['/admin/orders']);
+    } else if (componentName === 'categories') {
+      this.router.navigate(['/admin/categories']);
+    } else if (componentName === 'products') {
+      this.router.navigate(['/admin/products']);
+    } else if (componentName === 'users') {
+      this.router.navigate(['/admin/users']);
+    }
+  }
+
 }
