3cba88795f63aa82903598ecf4208f885302adb7
hoangbv1406 <hoangbv.dev@gmail.com>
2025-10-31 10:00:00 +0700

    feat: add user popover with actions and styles

--- shopapp-frontend/src/app/components/header/header.component.ts
+++ shopapp-frontend/src/app/components/header/header.component.ts
@@ -1,14 +1,45 @@
 // File: header.component.ts
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { BaseComponent } from '../base/base.component';
+import { UserResponse } from '../../responses/user.response';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, NgbModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent extends BaseComponent implements OnInit {
+  userResponse?: UserResponse | null;
+  isPopoverOpen = false;
+  activeNavItem: number = 0;
+
+  ngOnInit() {
+    this.userResponse = this.userService.getUserResponseFromLocalStorage();
+  }
+
+  togglePopover(event: Event): void {
+    event.preventDefault();
+    this.isPopoverOpen = !this.isPopoverOpen;
+  }
+
+  handleItemClick(index: number): void {
+    if (index === 0) {
+      this.router.navigate(['/user-profile']);
+    } else if (index === 2) {
+      this.tokenService.removeToken();
+      this.userService.removeUserFromLocalStorage();
+      this.userResponse = this.userService.getUserResponseFromLocalStorage();
+    }
+    this.isPopoverOpen = false;
+  }
+
+  setActiveNavItem(index: number) {
+    this.activeNavItem = index;
+  }
+
 }
