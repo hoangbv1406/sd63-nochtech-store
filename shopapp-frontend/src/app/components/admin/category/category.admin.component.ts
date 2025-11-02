fb7b66985476fc65f85f866a5332aa946d2ea8aa
hoangbv1406 <hoangbv.dev@gmail.com>
2025-11-02 10:00:00 +0700

    feat: move router-outlet into content column and add category list

--- shopapp-frontend/src/app/components/admin/category/category.admin.component.ts
+++ shopapp-frontend/src/app/components/admin/category/category.admin.component.ts
@@ -1,7 +1,11 @@
 // File: category.admin.component.ts
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { BaseComponent } from '../../base/base.component';
+import { Category } from '../../../models/category';
+import { ApiResponse } from '../../../responses/api.response';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-category-admin',
@@ -10,5 +14,24 @@ import { RouterModule } from '@angular/router';
   templateUrl: './category.admin.component.html',
   styleUrl: './category.admin.component.scss'
 })
-export class CategoryAdminComponent {
+export class CategoryAdminComponent extends BaseComponent implements OnInit {
+  categories: Category[] = [];
+
+  ngOnInit() {
+    this.getCategories(0, 100);
+  }
+
+  getCategories(page: number, limit: number) {
+    this.categoryService.getCategories(page, limit).subscribe({
+      next: (apiResponse: ApiResponse) => { this.categories = apiResponse.data },
+      complete: () => { debugger },
+      error: (error: HttpErrorResponse) => {
+        this.toastService.showToast({
+          error: error,
+          defaultMsg: 'Lỗi tải danh sách danh mục',
+          title: 'Lỗi Danh Mục'
+        });
+      }
+    });
+  }
 }
