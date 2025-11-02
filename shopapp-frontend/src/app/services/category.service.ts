a727f87d59517f9b51ba393907db2abd3abc5f03
hoangbv1406 <hoangbv.dev@gmail.com>
2025-11-02 10:00:00 +0700

    feat: add update-category form, component logic and service method

--- shopapp-frontend/src/app/services/category.service.ts
+++ shopapp-frontend/src/app/services/category.service.ts
@@ -5,6 +5,7 @@ import { environment } from '../../environments/environment';
 import { ApiResponse } from '../responses/api.response';
 import { Observable } from 'rxjs/internal/Observable';
 import { InsertCategoryDTO } from '../dtos/category/insert.category.dto';
+import { UpdateCategoryDTO } from '../dtos/category/update.category.dto';
 
 @Injectable({
   providedIn: 'root'
@@ -20,7 +21,16 @@ export class CategoryService {
     return this.http.get<ApiResponse>(`${environment.apiBaseUrl}/categories`, { params });
   }
 
+  getDetailCategory(id: number): Observable<ApiResponse> {
+    return this.http.get<ApiResponse>(`${this.apiBaseUrl}/categories/${id}`);
+  }
+
   insertCategory(insertCategoryDTO: InsertCategoryDTO): Observable<ApiResponse> {
     return this.http.post<ApiResponse>(`${this.apiBaseUrl}/categories`, insertCategoryDTO);
   }
+
+  updateCategory(id: number, updatedCategory: UpdateCategoryDTO): Observable<ApiResponse> {
+    return this.http.put<ApiResponse>(`${this.apiBaseUrl}/categories/${id}`, updatedCategory);
+  }
+
 }
