0659b7f1c2b19f74aa51441159b29449edbd2414
hoangbv1406 <hoangbv.dev@gmail.com>
2025-11-04 10:00:00 +0700

    feat: add createPaymentUrl method to PaymentService

--- shopapp-frontend/src/app/services/payment.service.ts
+++ shopapp-frontend/src/app/services/payment.service.ts
@@ -2,6 +2,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { ApiResponse } from '../responses/api.response';
+import { CreatePaymentDTO } from '../dtos/payment/create.payment.dto';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,4 +12,9 @@ import { environment } from '../../environments/environment';
 export class PaymentService {
   private apiBaseUrl = environment.apiBaseUrl;
   constructor(private http: HttpClient) { }
+
+  createPaymentUrl(paymentData: CreatePaymentDTO): Observable<ApiResponse> {
+    return this.http.post<ApiResponse>(`${this.apiBaseUrl}/payments/create_payment_url`, paymentData);
+  }
+
 }
