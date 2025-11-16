5cb22eadba6e12e4f6cbbd964c544cc9d2cd2511
hoangbv1406 <hoangbv.dev@gmail.com>
2025-11-16 10:00:00 +0700

    feat: update Category to support hierarchy and unique names

--- shopapp-backend/src/main/java/com/project/shopapp/models/Category.java
+++ shopapp-backend/src/main/java/com/project/shopapp/models/Category.java
@@ -1,7 +1,11 @@
 package com.project.shopapp.models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.List;
 
 @Entity
 @Table(name = "categories")
@@ -17,7 +21,20 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "name", nullable = false, unique = true)
     private String name;
+
+    @Column(name = "slug")
+    private String slug;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    @ToString.Exclude
+    @JsonBackReference
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private List<Category> subCategories;
 
 }
