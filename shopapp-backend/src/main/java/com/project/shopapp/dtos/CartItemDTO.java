2346c8985fb73139901c5658d65d7bc6c737693d
hoangbv1406 <hoangbv.dev@gmail.com>
2025-11-17 10:00:00 +0700

    feat: add variant support and quantity validation to CartItemDTO

--- shopapp-backend/src/main/java/com/project/shopapp/dtos/CartItemDTO.java
+++ shopapp-backend/src/main/java/com/project/shopapp/dtos/CartItemDTO.java
@@ -1,6 +1,7 @@
 package com.project.shopapp.dtos;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
 import lombok.*;
 
 @NoArgsConstructor
@@ -14,6 +15,10 @@ public class CartItemDTO {
     @JsonProperty("product_id")
     private Long productId;
 
+    @JsonProperty("variant_id")
+    private Long variantId;
+
+    @Min(value = 1, message = "Quantity must be at least 1")
     @JsonProperty("quantity")
     private Integer quantity;
 
