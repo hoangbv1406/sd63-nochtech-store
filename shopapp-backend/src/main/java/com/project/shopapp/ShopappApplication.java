9f064bf74bec7acf392a38e6d5061502dc568a98
hoangbv1406 <hoangbv.dev@gmail.com>
2025-11-23 10:00:00 +0700

    config: enable JPA auditing, scheduling, and async processing

--- shopapp-backend/src/main/java/com/project/shopapp/ShopappApplication.java
+++ shopapp-backend/src/main/java/com/project/shopapp/ShopappApplication.java
@@ -1,23 +1,19 @@
 package com.project.shopapp;
 
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableScheduling
+@EnableAsync
 public class ShopappApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ShopappApplication.class, args);
-    }
-
-    @Value("${jwt.secretKey}")
-    private String jwtKey;
-
-    @PostConstruct
-    public void Test() {
-        System.out.println("jwtKey: " + jwtKey);
     }
 
 }
