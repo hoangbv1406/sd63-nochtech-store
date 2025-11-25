ed2531f8e77dd6f00e9e984f4f0d212fb5f54a84
hoangbv1406 <hoangbv.dev@gmail.com>
2025-11-25 10:00:00 +0700

    feat: add Notification entity

---
+++ shopapp-backend/src/main/java/com/project/shopapp/models/Notification.java
@@ -0,0 +1,36 @@
+package com.project.shopapp.models;
+
+import com.project.shopapp.enums.NotificationType;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "notifications")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Notification extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private String title;
+    private String body;
+
+    @Enumerated(EnumType.STRING)
+    private NotificationType type;
+
+    @Column(name = "reference_id")
+    private String referenceId;
+
+    @Column(name = "is_read")
+    private boolean isRead;
+
+}
