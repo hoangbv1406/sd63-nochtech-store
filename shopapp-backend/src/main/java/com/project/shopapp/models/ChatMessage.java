d480af64db45b5c2e5b494f424c21cd95edf1b97
hoangbv1406 <hoangbv.dev@gmail.com>
2025-11-25 10:00:00 +0700

    feat: add ChatMessage entity

---
+++ shopapp-backend/src/main/java/com/project/shopapp/models/ChatMessage.java
@@ -0,0 +1,40 @@
+package com.project.shopapp.models;
+
+import com.project.shopapp.enums.ChatMessageType;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "chat_messages")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "room_id", nullable = false)
+    private ChatRoom chatRoom;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id", nullable = false)
+    private User sender;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    private ChatMessageType type;
+
+    @Column(name = "attachment_url", columnDefinition = "json")
+    private String attachmentUrl;
+
+    @Column(name = "is_read")
+    private boolean isRead;
+
+}
