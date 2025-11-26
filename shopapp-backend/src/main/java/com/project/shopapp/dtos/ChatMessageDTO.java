3ae94773008c34e3dc81a26d02af054561c25fe3
hoangbv1406 <hoangbv.dev@gmail.com>
2025-11-26 10:00:00 +0700

    feat: add ChatMessageDTO for message payloads and attachments

---
+++ shopapp-backend/src/main/java/com/project/shopapp/dtos/ChatMessageDTO.java
@@ -0,0 +1,27 @@
+package com.project.shopapp.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.project.shopapp.enums.ChatMessageType;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChatMessageDTO {
+
+    @JsonProperty("room_id")
+    @NotNull(message = "Room ID is required")
+    private Long roomId;
+
+    private String content;
+
+    @Builder.Default
+    private ChatMessageType type = ChatMessageType.TEXT;
+
+    @JsonProperty("attachment_url")
+    private String attachmentUrl;
+
+}
